8d671bd4a50f231477b0a231d39f1bb40dcc607f
Jose Chavira <joeychavira2016@gmail.com>
2019-10-13 20:46:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest_key = 0
- lowest_value = 0
- hash.each do |k, v|
+ lowest_value = nil
+ hash.each do |key, value|
    if lowest_value == 0 || v < lowest_value
-     lowest_value = v
-     lowest_key = k
+     lowest_value = value
+     lowest_key = key
    end
  end
  lowest_key
